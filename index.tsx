339c5cb4cf8a1ace963c4bc7599afde23b06e0ab
Snuggs <osnugglebunnyo@gmail.com>
2026-08-03 20:14:48 -0400

    restore author credit so the plugin links to my Discord profile
    What changed
    The author ID in the plugin metadata was a placeholder zero. It is now
    the real ID, matching the copy that has been running locally all along.
    
    Why we did this
    Vencord uses that ID to show who made a plugin. With a zero there, the
    credit in the plugin list did not link to anything. Now that other
    people will be installing these, it should point somewhere real.

--- index.tsx
+++ index.tsx
@@ -182,7 +182,7 @@ function animationLoop(i = 0) {
 export default definePlugin({
     name: "AnimatedStatus",
     description: "Cycles your custom status through a list of text/emoji steps on a timer.",
-    authors: [{ name: "isnuggs", id: 0n }],
+    authors: [{ name: "isnuggs", id: 1483261914392825958n }],
     dependencies: ["UserSettingsAPI"],
     settings,
 
